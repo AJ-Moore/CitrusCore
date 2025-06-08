796c4987a9e59a285ba8c6798bc4fcc0eb2d3d53
Alzon <allan.j.moore@outlook.com>
2025-06-08 22:36:49 +0100

    Resource manager tweaks.

--- source/Resources/AResource.h
+++ source/Resources/AResource.h
@@ -1,20 +1,31 @@
 #pragma once
 
+//#include "Resources/ResourceHandle.h"
 #include <CCCommon.h>
 
 namespace CitrusCore
 {
-	class CITRUS_CORE_API AResourceBase{};
+	class CITRUS_CORE_API AResourceBase{
+	public:
+		virtual bool LoadResource(std::string path) = 0;
+		virtual void UnloadResource() = 0;
+		virtual void SetResourceLocation(const std::string& path) = 0;
+	};
+
+	template <typename T>
+	class ResourceHandle;
 
 	template <class T>
 	class CITRUS_CORE_API AResource : public AResourceBase
 	{
+		friend class ResourceHandle<T>;
 	public:
-		virtual ~AResource() { }
+		virtual ~AResource() { UnloadResource(); }
 		const std::string& GetResourceLocation() { return m_resourceLocation; }
 	protected:
-		virtual bool LoadResource(std::string path) = 0;
-		void SetResourceLocation(const std::string& path) { m_resourceLocation = path; }
+		virtual bool LoadResource(std::string path) override = 0;
+		virtual void UnloadResource() override = 0;
+		virtual void SetResourceLocation(const std::string& path) override final { m_resourceLocation = path; }
 	private:
 		std::string m_resourceLocation;
 	};
--- source/Resources/ResourceHandle.h
+++ source/Resources/ResourceHandle.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include <Resources/AResource.h>
 #include <CCCommon.h>
 
 namespace CitrusCore
 {
+	template<class T> 
+	class ResourceGroup;
+
 	template <class T>
 	class CITRUS_CORE_API ResourceHandle
 	{
-		//friend class ResourceGroup;
+		static_assert(std::is_base_of<AResource<T>, T>::value, 
+			"T must derive from AResource");
+		friend class ResourceGroup<T>;
 	public:
 		ResourceHandle() = delete;
 		ResourceHandle(std::string path) { m_path = path; }
-		std::weak_ptr<T> GetResource() { return m_resource; }
+		T* GetResource() { return m_resource.get(); }
+	protected:
+		virtual void Load(); 
+		virtual void Unload();
 	private: 
 		std::string m_path;
 		std::unique_ptr<T> m_resource = nullptr;
@@ -20,4 +29,18 @@ namespace CitrusCore
 
 	template <class T>
 	using ResourcePtr = std::shared_ptr<ResourceHandle<T>>;
+
+	template<class T>
+	void ResourceHandle<T>::Load() 
+	{
+		AResource<T>* resource = (AResource<T>*)GetResource();
+		resource->LoadResource(m_path);
+	}
+
+	template<class T>
+	void ResourceHandle<T>::Unload() 
+	{
+		AResource<T>* resource = (AResource<T>*)GetResource();
+		resource->UnloadResource();
+	}
 }
